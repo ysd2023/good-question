5f712d1a924cc57adb2f356223b66d5a51d9bd5a
ysd <3043162910@qq.com>
2022-07-27 12:15:21 +0800

    初始化项目代码

---
+++ src/main/java/com/dongliu/goodquestion/GoodQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.dongliu.goodquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodQuestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodQuestionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dongliu/goodquestion/GoodQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongliu.goodquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodQuestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
