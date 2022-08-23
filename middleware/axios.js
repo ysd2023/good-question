cedb84d806fa11c7c07bf0c92b6155ef4e81f58f
ysd <3043162910@qq.com>
2022-08-23 18:16:29 +0800

    修改后端服务器地址及接口

--- middleware/axios.js
+++ middleware/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://192.168.2.202:3000/'
 // axios.defaults.baseURL = 'http://localhost:5000/'
-axios.defaults.baseURL = 'http://192.168.2.202:8000'
+//axios.defaults.baseURL = 'http://192.168.2.202:8000'
+axios.defaults.baseURL = 'http://localhost:88888'
 
 export default axios
