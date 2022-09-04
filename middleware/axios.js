f5fdf72ae090c417bc80bc8de4a03e6ad866fde7
hongshaoxia927 <2685703961@qq.com>
2022-09-04 10:30:56 +0800

    修改axios默认url

--- middleware/axios.js
+++ middleware/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://192.168.2.202:3000/'
 // axios.defaults.baseURL = 'http://localhost:5000/'
-axios.defaults.baseURL = 'http://192.168.2.202:8000'
+axios.defaults.baseURL = 'http://10.3.114.39:8000'
 
 export default axios
